8c5c9d759fe7001376dc57b5aef6d789f5880de5
Renan Lopes Ferreira <relferreira@gmail.com>
2017-10-14 09:18:02 -0300

    Project inicialization

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
